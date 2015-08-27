1a05c6f5b9d2d22eb959f5434614161a4f993420
UndefinedOffset <echipman@webbuildersgroup.com>
2015-08-27 14:40:58 -0300

    Added loading spinner for the logs list when loading a new page
    Fixed some minor style issues with the logs list

--- javascript/KapostBridgeLogViewer.js
+++ javascript/KapostBridgeLogViewer.js
@@ -9,6 +9,9 @@
         $('.KapostBridgeLogViewer .cms-content-tools .cms-panel-content .logs-pagination .more-logs-link').entwine({
             onclick: function(e) {
                 var self=$(this);
+                var panel=$(this).closest('.cms-content-tools');
+                
+                panel.addClass('loading');
                 
                 $.ajax({
                     url: self.attr('href'),
@@ -17,9 +20,13 @@
                     },
                     success: function(data) {
                         self.parent().replaceWith(data.LogEntries);
+                        
+                        panel.removeClass('loading');
                     },
                     error: function(xhr, status, error) {
                         $('.cms-container').trigger('loadfragmenterror', {xhr: xhr, status: status, error: error});
+                        
+                        panel.removeClass('loading');
                     }
                 });
                 
